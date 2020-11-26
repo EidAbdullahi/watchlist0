5414882cbd35286cf53a45569da2c793e774f33a
EidAbdullahi <eidabdullahi10@gmail.com>
2020-11-27 00:48:06 +0300

    commit all changes

---
+++ app/config.py
@@ -0,0 +1,27 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/550?api_key=dc8778f07dfcbb76925d7106d12b5dfb'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
---
+++ instance/config.py
@@ -0,0 +1,3 @@
+MOVIE_API_KEY = 'dc8778f07dfcbb76925d7106d12b5dfb'
+MOVIE_API_KEY = '<Movie API Key>'
+SECRET_KEY = '<Flask WTF Secret Key>'
